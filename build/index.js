5328f34e82584f4782a3d2e92b55c3e7abbb075b
Sergey Chaliy <chaliysergey@ya.ru>
2017-09-05 20:03:26 +0300

    initial commit

---
+++ build/index.js
@@ -0,0 +1,228 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _stringify = require('babel-runtime/core-js/json/stringify');
+
+var _stringify2 = _interopRequireDefault(_stringify);
+
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = require('babel-runtime/helpers/createClass');
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+var _axios = require('axios');
+
+var _axios2 = _interopRequireDefault(_axios);
+
+var _cryptoJs = require('crypto-js');
+
+var _cryptoJs2 = _interopRequireDefault(_cryptoJs);
+
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var isMissing = function isMissing(param) {
+  throw new Error('Missing parameter "' + param + '"');
+};
+
+var BFXRest = function () {
+
+  /**
+   * constructor
+   *
+   * @param {object} required (API_KEY, API_SECRET)
+   */
+  function BFXRest(_ref) {
+    var API_KEY = _ref.API_KEY,
+        API_SECRET = _ref.API_SECRET;
+    (0, _classCallCheck3.default)(this, BFXRest);
+
+    this.apiKey = API_KEY;
+    this.apiSecret = API_SECRET;
+    this.version = 'v1';
+    this.baseUrl = 'https://api.bitfinex.com';
+  }
+
+  /**
+   * _nonce - Nonce generator
+   *
+   * @return {number}  Incremented nonce
+   */
+
+
+  (0, _createClass3.default)(BFXRest, [{
+    key: '_nonce',
+    value: function _nonce() {
+      return new Date().getTime();
+    }
+
+    /**
+     * _authReq - Make request params and headers
+     *
+     * @param  {string} path    Path to api endpoint
+     * @param  {object} params  Params for post request
+     * @return {promise}        Request promise
+     */
+
+  }, {
+    key: '_authReq',
+    value: function _authReq(path, params) {
+      var url = this.baseUrl + '/' + this.version + '/' + path;
+
+      var body = {
+        request: '/' + this.version + '/' + path,
+        nonce: (0, _stringify2.default)(this._nonce())
+      };
+
+      if (!_lodash2.default.isEmpty(params)) {
+        _lodash2.default.map(params, function (paramVal, paramKey) {
+          body[paramKey] = paramVal;
+        });
+      }
+
+      var payload = new Buffer((0, _stringify2.default)(body)).toString('base64');
+
+      var signature = _cryptoJs2.default.HmacSHA384(payload, this.apiSecret).toString(_cryptoJs2.default.enc.Hex);
+
+      var headers = {
+        'X-BFX-APIKEY': this.apiKey,
+        'X-BFX-PAYLOAD': payload,
+        'X-BFX-SIGNATURE': signature
+      };
+
+      return _axios2.default.post(url, {}, {
+        headers: headers
+      }).then(function (_ref2) {
+        var data = _ref2.data;
+
+        if (data.message) {
+          throw new Error(data.message);
+        }
+
+        return data;
+      });
+    }
+
+    /**
+     * balance - Current balance of wallets
+     *
+     * @return {array}  Array of wallets
+     */
+
+  }, {
+    key: 'walletBalance',
+    value: function walletBalance() {
+      return this._authReq('balances');
+    }
+  }, {
+    key: 'newOrder',
+    value: function newOrder() {
+      var _ref3 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+          _ref3$symbol = _ref3.symbol,
+          symbol = _ref3$symbol === undefined ? isMissing('symbol') : _ref3$symbol,
+          _ref3$amount = _ref3.amount,
+          amount = _ref3$amount === undefined ? isMissing('amount') : _ref3$amount,
+          _ref3$price = _ref3.price,
+          price = _ref3$price === undefined ? isMissing('price') : _ref3$price,
+          _ref3$exchange = _ref3.exchange,
+          exchange = _ref3$exchange === undefined ? isMissing('exchange') : _ref3$exchange,
+          _ref3$side = _ref3.side,
+          side = _ref3$side === undefined ? isMissing('side') : _ref3$side,
+          _ref3$type = _ref3.type,
+          type = _ref3$type === undefined ? isMissing('type') : _ref3$type,
+          _ref3$post_only = _ref3.post_only,
+          post_only = _ref3$post_only === undefined ? false : _ref3$post_only,
+          _ref3$is_hidden = _ref3.is_hidden,
+          is_hidden = _ref3$is_hidden === undefined ? false : _ref3$is_hidden;
+
+      var params = {
+        symbol: symbol,
+        amount: amount,
+        price: price,
+        exchange: exchange,
+        side: side,
+        type: type,
+        post_only: post_only,
+        is_hidden: is_hidden
+      };
+
+      // return this._authReq('order/new', params);
+
+      console.log('params', params);
+    }
+  }, {
+    key: 'newMultipleOrders',
+    value: function newMultipleOrders() {
+      var _ref4 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+          _ref4$orders = _ref4.orders,
+          orders = _ref4$orders === undefined ? isMissing('orders') : _ref4$orders;
+
+      if (_lodash2.default.isEmpty(orders) || !_lodash2.default.isArray(orders)) {
+        throw new Error('Orders is empty or is not array');
+      }
+
+      var params = {
+        orders: orders
+      };
+
+      return this._authReq('order/new/multi', params);
+    }
+  }, {
+    key: 'cancelOrder',
+    value: function cancelOrder() {
+      var _ref5 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+          _ref5$order_id = _ref5.order_id,
+          order_id = _ref5$order_id === undefined ? isMissing('order_id') : _ref5$order_id;
+
+      var params = {
+        order_id: order_id
+      };
+
+      return this._authReq('order/cancel', params);
+    }
+  }, {
+    key: 'cancelMultipleOrders',
+    value: function cancelMultipleOrders() {
+      var _ref6 = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+          _ref6$orders = _ref6.orders,
+          orders = _ref6$orders === undefined ? isMissing('orders') : _ref6$orders;
+
+      var params = {
+        order_ids: _lodash2.default.map(orders, function (order) {
+          return +order.id;
+        })
+      };
+
+      return this._authReq('order/cancel/multi', params);
+    }
+  }, {
+    key: 'cancelAllOrders',
+    value: function cancelAllOrders() {
+      return this._authReq('order/cancel/all');
+    }
+  }, {
+    key: 'activeOrders',
+    value: function activeOrders() {
+      return this._authReq('orders');
+    }
+  }, {
+    key: 'ordersHistory',
+    value: function ordersHistory() {
+      return this._authReq('orders/hist');
+    }
+  }]);
+  return BFXRest;
+}();
+
+;
+
+exports.default = BFXRest;
---
+++ src/index.js
@@ -0,0 +1,165 @@
+import axios from 'axios';
+import crypto from 'crypto-js';
+import _ from 'lodash';
+
+const isMissing = (param) => {
+    throw new Error(`Missing parameter "${param}"`);
+};
+
+class BFXRest {
+
+  /**
+   * constructor
+   *
+   * @param {object} required (API_KEY, API_SECRET)
+   */
+  constructor({
+    API_KEY,
+    API_SECRET
+  }) {
+    this.apiKey = API_KEY;
+    this.apiSecret = API_SECRET;
+    this.version = 'v1';
+    this.baseUrl = 'https://api.bitfinex.com';
+  }
+
+
+  /**
+   * _nonce - Nonce generator
+   *
+   * @return {number}  Incremented nonce
+   */
+  _nonce() {
+    return new Date().getTime();
+  }
+
+
+  /**
+   * _authReq - Make request params and headers
+   *
+   * @param  {string} path    Path to api endpoint
+   * @param  {object} params  Params for post request
+   * @return {promise}        Request promise
+   */
+  _authReq(path, params) {
+    const url = `${this.baseUrl}/${this.version}/${path}`;
+
+    const body = {
+      request: `/${this.version}/${path}`,
+      nonce: JSON.stringify(this._nonce())
+    };
+
+    if (!_.isEmpty(params)) {
+      _.map(params, (paramVal, paramKey) => { body[paramKey] = paramVal; })
+    }
+
+    const payload = new Buffer(JSON.stringify(body)).toString('base64');
+
+    const signature = crypto.HmacSHA384(payload, this.apiSecret).toString(crypto.enc.Hex);
+
+    const headers = {
+      'X-BFX-APIKEY': this.apiKey,
+      'X-BFX-PAYLOAD': payload,
+      'X-BFX-SIGNATURE': signature
+    };
+
+    return axios.post(url, {}, {
+      headers
+    })
+    .then(({ data }) => {
+      if (data.message) {
+        throw new Error(data.message);
+      }
+
+      return data;
+    });
+  }
+
+
+  /**
+   * balance - Current balance of wallets
+   *
+   * @return {array}  Array of wallets
+   */
+  walletBalance() {
+    return this._authReq('balances');
+  }
+
+  newOrder({
+    symbol = isMissing('symbol'),
+    amount = isMissing('amount'),
+    price = isMissing('price'),
+    exchange = isMissing('exchange'),
+    side = isMissing('side'),
+    type = isMissing('type'),
+    post_only = false,
+    is_hidden = false
+  } = {}) {
+
+    const params = {
+      symbol,
+      amount,
+      price,
+      exchange,
+      side,
+      type,
+      post_only,
+      is_hidden
+    };
+
+    // return this._authReq('order/new', params);
+
+    console.log('params', params);
+  }
+
+  newMultipleOrders({
+    orders = isMissing('orders')
+  } = {}) {
+
+    if (_.isEmpty(orders) || !_.isArray(orders)) {
+      throw new Error('Orders is empty or is not array');
+    }
+
+    const params = {
+      orders
+    };
+
+    return this._authReq('order/new/multi', params);
+  }
+
+  cancelOrder({
+    order_id = isMissing('order_id')
+  } = {}) {
+
+    const params = {
+      order_id
+    };
+
+    return this._authReq('order/cancel', params);
+  }
+
+  cancelMultipleOrders({
+    orders = isMissing('orders')
+  } = {}) {
+
+    const params = {
+      order_ids: _.map(orders, order => +order.id)
+    };
+
+    return this._authReq('order/cancel/multi', params);
+  }
+
+  cancelAllOrders() {
+    return this._authReq('order/cancel/all');
+  }
+
+  activeOrders() {
+    return this._authReq('orders');
+  }
+
+  ordersHistory() {
+    return this._authReq('orders/hist');
+  }
+};
+
+export default BFXRest;
